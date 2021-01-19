6a37cf09d247f00d3a9d07e15bc51524b6f5ad74
YAŞAR <YAAR@DESKTOP-NPJQQAG>
2021-01-20 02:39:53 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp4/metot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class metot
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyatı = 15;
+            string acıklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elamsı";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Dıyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------");
+            }
+
+        }
+    }
+
+    }
+}
